6e62a30604230681f9a52055baaa32781455c477
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-09 17:23:52 -0600

    +

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from '@/components/auth/sign-up-form';
+
+export default async function SignUp() {
+  return (
+    <div className='w-[400px] mx-auto mt-20'>
+      <SignUpForm />
+    </div>
+  );
+}
