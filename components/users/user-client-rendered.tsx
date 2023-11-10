d4dfe29a5d460b4ce1b4bda524898eded08d4ffa
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-09 20:33:46 -0600

    +

--- components/users/user-client-rendered.tsx
+++ components/users/user-client-rendered.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { User } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 import { FC } from 'react';
 
@@ -8,7 +9,10 @@ export const UserClientRendered: FC = () => {
 
   return (
     <div className='bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2'>
-      <p className='text-gray-500 font-semibold'>user (client rendered)</p>
+      <div className='grid grid-cols-[max-content_auto] gap-1 place-items-center w-max text-zinc-500'>
+        <User className='w-4 h-4' />
+        <span>user (client rendered)</span>
+      </div>
       <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
--- components/users/user-server-rendered.tsx
+++ components/users/user-server-rendered.tsx
@@ -1,4 +1,5 @@
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { User } from 'lucide-react';
 import { getServerSession } from 'next-auth';
 import { FC } from 'react';
 
@@ -7,7 +8,10 @@ export const UserServerRendered: FC = async () => {
 
   return (
     <div className='bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2'>
-      <p className='text-gray-500 font-semibold'>user (server rendered)</p>
+      <div className='grid grid-cols-[max-content_auto] gap-1 place-items-center w-max text-zinc-500'>
+        <User className='w-4 h-4' />
+        <span>user (server rendered)</span>
+      </div>
       <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
