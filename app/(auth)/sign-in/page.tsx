9f8c9c48edb29edfa7f95ddc2a2675e760735de2
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-09 19:54:48 -0600

    +

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,11 @@
 import { SignInForm } from '@/components/auth/sign-in-form';
 import type { NextPage } from 'next';
 
-const SignInPage: NextPage = async () => {
+interface SignInPageProps {
+  searchParams?: Record<'callbackUrl' | 'error', string>;
+}
+
+const SignInPage: NextPage<SignInPageProps> = async ({ searchParams }) => {
   return (
     <div className='w-[400px] mx-auto mt-20'>
       <SignInForm />
--- components/auth/sign-in-form.tsx
+++ components/auth/sign-in-form.tsx
@@ -1,34 +1,31 @@
 'use client';
 
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import * as z from 'zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
 import { LucideLoader } from 'lucide-react';
 import { signIn } from 'next-auth/react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const formSchema = z.object({
-  name: z.string(),
   email: z.string(),
   password: z.string()
 });
 
 export const SignInForm: FC = () => {
   const router = useRouter();
-  const [nameMessage, setNameMessage] = useState('');
-  const [emailMessage, setEmailMessage] = useState('');
-  const [passwordMessage, setPasswordMessage] = useState('');
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+  const [error, setError] = useState('');
 
   const [formValid, setFormValid] = useState({ name: false, email: false, password: false });
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: '',
       email: '',
       password: ''
     }
@@ -38,61 +35,23 @@ export const SignInForm: FC = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      // const data = await axios.post('/api/auth/callback/credentials', {
-      //   name: 'asd',
-      //   password: 'asd'
-      // });
+      const res = await signIn('credentials', { email: values.email, password: values.password, redirect: false, callbackUrl });
 
-      // console.log(data);
-
-      console.log('sign in test');
-      await signIn('credentials', { email: 'asd@asd.com', password: 'asd', redirect: true, callbackUrl: 'http://localhost:3000' }).then(error => console.log(error));
+      if (res?.ok) {
+        router.push(callbackUrl);
+      } else {
+        setError('Authentication failed');
+      }
     } catch (err) {
       console.log('register error', err);
     }
   };
 
-  useEffect(() => {
-    setNameMessage('Name is required');
-    setEmailMessage('Email is required');
-    setPasswordMessage('Password is required');
-  }, []);
-
   return (
     <div className='bg-neutral-200 dark:bg-neutral-900 sm:rounded-md p-2'>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
-          <FormField
-            control={form.control}
-            name='name'
-            render={({ field }) => (
-              <FormItem className='grid grid-flow-row'>
-                <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>Username</FormLabel>
-                <FormControl>
-                  <input
-                    disabled={isLoading}
-                    className='bg-zinc-300/50 dark:bg-neutral-800 border-0 focus-visible:ring-0 text-black dark:text-white outline-none p-2 rounded-md resize-none'
-                    placeholder='Enter username'
-                    {...field}
-                    onChange={e => {
-                      const value = e.target.value;
-
-                      if (value.length === 0) {
-                        setNameMessage('Name is required');
-                        setFormValid({ ...formValid, name: false });
-                      } else {
-                        setNameMessage('');
-                        setFormValid({ ...formValid, name: true });
-                      }
-
-                      field.onChange(e);
-                    }}
-                  />
-                </FormControl>
-                <FormMessage>{nameMessage}</FormMessage>
-              </FormItem>
-            )}
-          />
+          <FormMessage>{error}</FormMessage>
           <FormField
             control={form.control}
             name='email'
@@ -106,21 +65,11 @@ export const SignInForm: FC = () => {
                     placeholder='Enter email'
                     {...field}
                     onChange={e => {
-                      const value = e.target.value;
-
-                      if (value.length === 0) {
-                        setEmailMessage('Email is required');
-                        setFormValid({ ...formValid, email: false });
-                      } else {
-                        setNameMessage('');
-                        setFormValid({ ...formValid, email: true });
-                      }
-
+                      setError('');
                       field.onChange(e);
                     }}
                   />
                 </FormControl>
-                <FormMessage>{emailMessage}</FormMessage>
               </FormItem>
             )}
           />
@@ -137,30 +86,16 @@ export const SignInForm: FC = () => {
                     placeholder='Enter password'
                     {...field}
                     onChange={e => {
-                      const value = e.target.value;
-
-                      if (value.length === 0) {
-                        setPasswordMessage('Password is required');
-                        setFormValid({ ...formValid, password: false });
-                      } else {
-                        setNameMessage('');
-                        setFormValid({ ...formValid, password: true });
-                      }
-
+                      setError('');
                       field.onChange(e);
                     }}
                   />
                 </FormControl>
-                <FormMessage>{passwordMessage}</FormMessage>
               </FormItem>
             )}
           />
           <div>
-            <Button
-              // disabled={isLoading || !(formValid.name && formValid.email && formValid.password)}
-              className='bg-emerald-500 text-white hover:bg-emerald-800 transition w-[80px]'>
-              {isLoading ? <LucideLoader /> : <p>Sign in</p>}
-            </Button>
+            <Button className='bg-emerald-500 text-white hover:bg-emerald-800 transition w-[80px]'>{isLoading ? <LucideLoader /> : <p>Sign in</p>}</Button>
           </div>
         </form>
       </Form>
